4892c68e749b8a9f9a6974dca048bfeda0947f36
Masumesk <msumsanatkar@gmail.com>
2025-12-18 20:36:26 +0330

    fix : batmobil moving area and adding comment

--- Batman/Assets/Scripts/Batmobil.cs
+++ Batman/Assets/Scripts/Batmobil.cs
@@ -2,63 +2,70 @@ using UnityEngine;
 
 public class Batmobil : MonoBehaviour
 {
-    public float runMultiplier = 2f;
-    public float yposfixed = -1.5f;
+    [SerializeField]
+    private float boost = 2f;
 
     private SpriteRenderer sr;
-    private float leftLimit;
-    private float rightLimit;
 
-    public characterstate stateReceiver;
+    [SerializeField]
+    private characterstate stateReceiver;
 
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-
-        float halfHeight = Camera.main.orthographicSize;
-        float halfWidth = halfHeight * Camera.main.aspect;
-
-        leftLimit = -halfWidth - 1f;
-        rightLimit = halfWidth + 1f;
-
         Vector3 pos = transform.position;
-        pos.y = yposfixed;
+        pos.y = -1.5f;
         transform.position = pos;
     }
 
     void Update()
     {
+        //inputs
         float moveX = Input.GetAxisRaw("Horizontal");
         float speed = stateReceiver.GetSpeed();
 
+        //boost
         if (Input.GetKey(KeyCode.LeftShift))
-            speed *= runMultiplier;
+        {
+            speed *= boost;
+        }
 
+        //move
         Vector2 movement = new Vector2(moveX, 0);
         transform.Translate(movement * speed * Time.deltaTime);
 
+        //ماشین فقط روی سطح زمین یعنی محور x حرکت میکند
         Vector3 pos = transform.position;
-        pos.y = yposfixed;
+        pos.y = -1.5f;
         transform.position = pos;
 
+        //نمایش تصویر ماشین با توجه به جهت حرکت
         if (moveX > 0)
+        {
             sr.flipX = true;
+        }
         else if (moveX < 0)
+        {
             sr.flipX = false;
+        }
 
         Wrap();
     }
 
+    //نمایش از سمت دیگر در صورت خروج از محدوده
     void Wrap()
     {
         Vector3 pos = transform.position;
 
-        if (pos.x > rightLimit)
-            pos.x = leftLimit;
-        else if (pos.x < leftLimit)
-            pos.x = rightLimit;
+        if (pos.x > 10.13f)
+        {
+            pos.x = -9.7f;
+        }
+        else if (pos.x < -9.7f)
+        {
+            pos.x = 10.13f;
+        }
 
-        pos.y = yposfixed;
         transform.position = pos;
     }
 }
