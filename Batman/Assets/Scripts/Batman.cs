be6a2ed544be6959eae0168ea886ac6834725655
Masumesk <msumsanatkar@gmail.com>
2025-12-18 20:29:34 +0330

    fix : fixing and adding comment to batman script

--- Batman/Assets/Scripts/Batman.cs
+++ Batman/Assets/Scripts/Batman.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Batman : MonoBehaviour
 {
-    public float runMultiplier = 2f;
+    [SerializeField]
+    private float boost = 2f;
 
     private SpriteRenderer sr;
-    public characterstate stateReceiver;
+    
+    [SerializeField]
+    private characterstate stateReceiver;
 
     void Start()
     {
@@ -15,23 +18,28 @@ public class Batman : MonoBehaviour
 
     void Update()
     {
+        //inputs
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-
+        
         float speed = stateReceiver.GetSpeed();
 
+        //سرعت بیشتر در صورت نگه داشتن shift
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            speed *= runMultiplier;
+            speed *= boost;
         }
 
+        //حرکت
         Vector2 movement = new Vector2(moveX, moveY);
         transform.Translate(movement * speed * Time.deltaTime);
 
         Vector3 pos = transform.position;
+        //محدود کردن محدوده حرکت عمودی
         pos.y = Mathf.Clamp(pos.y, -1.5f, 4f);
         transform.position = pos;
 
+        //طبق این که متحرک به کدام سمت حرکت میکند جهت تصویر بت من به همان سمت مشخص شود
         if (moveX > 0)
         {
             sr.flipX = true;
@@ -44,6 +52,7 @@ public class Batman : MonoBehaviour
         Wrap();
     }
 
+    //خارج نشدن بتمن از صفحه در صورت خروج از جپ از سمت راست نمایش داده میشود و برعکس ..
     void Wrap()
     {
         Vector3 pos = transform.position;
