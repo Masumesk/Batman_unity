8f15e9af619b8c625ba0025be75179367e233032
Masumesk <msumsanatkar@gmail.com>
2025-12-18 11:45:22 +0330

    add : controlling batmobil and batman

--- Batman/Assets/Scripts/CharacterManager.cs
+++ Batman/Assets/Scripts/CharacterManager.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject batman;
+    public GameObject Batmobil;
+
+   
+
     void Start()
     {
-        
+        batman.SetActive(true);
+        Batmobil.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            bool isBatmanActive = batman.activeSelf;
+
+            GameObject current = isBatmanActive ? batman : Batmobil;
+            GameObject next = isBatmanActive ? Batmobil : batman;
+
+           
+            Vector3 newPos = next.transform.position;
+            newPos.x = current.transform.position.x;
+
+            
+
+            next.transform.position = newPos;
+
+            
+            SpriteRenderer currentSR = current.GetComponent<SpriteRenderer>();
+            SpriteRenderer nextSR = next.GetComponent<SpriteRenderer>();
+
+            if (currentSR != null && nextSR != null)
+                nextSR.flipX = currentSR.flipX;
+
+            current.SetActive(false);
+            next.SetActive(true);
+        }
     }
 }
